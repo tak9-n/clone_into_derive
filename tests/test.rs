fdf96b60c1751260e6efc0ecae58df4971b1ad17
tak9-n <hirtak2000@gmail.com>
2023-01-04 00:34:55 +0900

    add test

--- tests/test.rs
+++ tests/test.rs
@@ -7,10 +7,13 @@ pub mod test_structs {
     pub struct IncludedStruct {
         pub a: i32,
         pub b: i32,
+        pub c: i32,
     }
     pub struct FullStruct {
-        pub a: i32,        pub b: i32,
+        pub a: i32,
+        pub b: i32,
         pub c: i32,
+        pub d: i32,
     }
 }
 
@@ -19,19 +22,23 @@ mod tests {
     
     #[test]
     fn clone_into() {
-        let included = super::test_structs::IncludedStruct {a: 0, b: 1};
-        let mut full = super::test_structs::FullStruct {a:2, b:3, c:4};
+        let included = super::test_structs::IncludedStruct {a: 0, b: 1, c: 2};
+        let mut full = super::test_structs::FullStruct {a:2, b:3, c:4, d: 5};
         included_struct_clone_into!(included, full);
         assert!(included.a == full.a);
         assert!(included.b == full.b);
+        assert!(included.c == full.c);
+        assert!(5 == full.d);
     }
 
     #[test]
     fn clone_into_new_struct() {
         use super::test_structs::FullStruct;
-        let included = super::test_structs::IncludedStruct {a: 0, b: 1};
-        let full = included_struct_clone_into!(included, FullStruct {c: 4});
+        let included = super::test_structs::IncludedStruct {a: 0, b: 1, c: 2};
+        let full = included_struct_clone_into!(included, FullStruct {d: 4});
         assert!(included.a == full.a);
         assert!(included.b == full.b);
+        assert!(included.c == full.c);
+        assert!(4 == full.d);
     }
 }
